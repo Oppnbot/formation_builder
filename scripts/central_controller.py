30d8a1bf91e9d26c960f941aee4c4a0ab153f84a
Tobias Recker <Recker@match.uni-hannover.de>
2024-05-29 15:59:47 +0200

    critical fix that prevents low prio robots from ignoring high prio robots at their goal pos

--- scripts/central_controller.py
+++ scripts/central_controller.py
@@ -135,7 +135,7 @@ if __name__ == '__main__':
     test_formation : Formation = Formation()
     test_formation.goal_poses = []
     #goal_positions : list[tuple[float, float]] = [(50, 20), (50, 19), (30, 18), (51.1, 20)]
-    goal_positions : list[tuple[float, float]] = [(35, 30), (35, 32.5)] #, (35, 35), (38.5, 30)]
+    goal_positions : list[tuple[float, float]] = [(33, 36), (33, 38.5) , (36, 38.5), (36, 36)] #[(35, 30), (35, 32.5) , (38.5, 32.5), (38.5, 30)]
     for index, goal_position in enumerate(goal_positions):
         test_formation.goal_poses.append(central_controller.generate_formation_position(index+1, goal_position, size=1.0))
 
